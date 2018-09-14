12dd3899dc20a86405c9b25e495d31cddc71ca86
杨益林 <1187148000@qq.com>
2018-09-14 17:33:04 +0800

    login-start

--- src/pages/login/components/Form.vue
+++ src/pages/login/components/Form.vue
@@ -1,5 +1,8 @@
 <template>
-	<div>表单</div>
+	<div class="content">
+		<input class="username" type="text" name="" value="" placeholder="">
+		<input class="password" type="password" name="" value="" placeholder="">
+	</div>
 </template>
 <script>
 	export default{
@@ -7,5 +10,26 @@
 	}
 </script>
 <style lang="stylus" scoped> 
-	
+	.content
+		 
+		justify-content:center
+		border:1px solid black
+		width:6rem
+		height:6rem
+		margin:3rem auto
+		.username
+			position:absolute
+			display:block
+			border:1px solid red
+			height:1rem
+			width:5rem
+			margin-top:2rem
+		.password
+			position:absolute
+			display:block
+			border:1px solid pink
+			height:1rem
+			width:5rem
+			margin-top:4rem
+		
 </style>
